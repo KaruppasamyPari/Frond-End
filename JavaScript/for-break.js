643fb561c69d2e5a929cabb8e034d7b4230a3fc9
Karuppasamy M <mkspari45@gmail.com>
2025-09-10 08:29:48 +0530

    js

--- JavaScript/for-break.js
+++ JavaScript/for-break.js
@@ -2,9 +2,23 @@
 // break,continue the loop execution
 // continue:pause the statement furthur stsatement
 // will get executed
+
+
 for(let i=0;i<=5;i++){
-    console.log(i)
-    if(i==3){
-        continue;
+    if(i==3) continue;{
+        console.log(i)
     }
+}
+
+
+// === value and datatype the value (strict equality operator)
+// == only value (loose equality operator)
+
+let x=10;
+let y=10;
+if(x===y){
+    console.log('the value and datatype are same')
+}
+else{
+    console.log('mismatch of both')  
 }
